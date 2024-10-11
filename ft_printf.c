c8c0b2c702e0a6a0ba1b4351748d0d02199ff002
Aritz Jaber Lopes <arjaber-@c3r8s4.42urduliz.com>
2024-10-11 17:49:50 +0200

    gestiona multiples % y varios espacios entre % y argumento

--- ft_printf.c
+++ ft_printf.c
@@ -6,12 +6,13 @@
 /*   By: arjaber- <arjaber-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 16:11:06 by arjaber-          #+#    #+#             */
-/*   Updated: 2024/10/11 17:07:07 by arjaber-         ###   ########.fr       */
+/*   Updated: 2024/10/11 17:49:29 by arjaber-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdio.h>
 
 void	ft_print_arg(char c, int *counter)
 {
@@ -19,26 +20,35 @@ void	ft_print_arg(char c, int *counter)
 	*counter = *counter + 1;
 }
 
-int	is_arg(char c)
+int	is_arg(const char *format, int *counter)
 {
-	int	counter;
-	char	*s;
+	int	 i;
+	char *s;
 
+	i = 0;
 	s = "cspdiuxX";
-	counter = 0;
-	while (s[counter])
+	while (format[*counter] && format[*counter] == ' ')
+		(*counter)++;
+	while (s[i])
 	{
-		if (s[counter] == c)
-			return (1);
-		counter++;
+		if (s[i] == format[*counter])
+		{
+			if (format[*counter - 1] && format[*counter - 1] == ' ')
+				write(1, " ", 1);
+			*counter = *counter - 1;
+			return (1);	
+		}
+		i++;
 	}
 	return (0);
 }
 
 int ft_printf(char const *format, ...)
 {
+	va_list	args;
 	int	counter;
 
+	va_start(args, format);
 	counter = 0;
 	if (!format)
 		return (1);
@@ -46,17 +56,24 @@ int ft_printf(char const *format, ...)
 	{
 		if (format[counter] != '%')
 			write(1, &format[counter], 1);
-		else if (format[counter] == '%' &&  is_arg(format[counter + 1]))
-			ft_print_arg(format[counter + 1], &counter);
-		else if (format[counter] == '%' &&  format[counter + 1] == '%')
-			write(1, "%", 1);
+		else if (format[counter] == '%')
+		{
+			counter++;
+			if (is_arg(format, &counter))
+				ft_print_arg(format[counter], &counter);
+			else if (format[counter] == '%')
+				write(1, "%", 1);
+		}
 		counter++;
 	}
+	va_end(args);
 	return (1);
 }
 
 int	main(void)
 {
-	ft_printf("Hola cómo estás, %s? Tienes un 20%% de posibilidades de ser majo :)");
+	ft_printf("Hola cómo estás, %s? Tienes un 20%%%     de posibilidades de ser majo :)");
+	printf("\n");
+	printf("Hola cómo estás, %s? Tienes un 20%%%     de posibilidades de ser majo :)", "[argumento]", 6);
 	return (0);
 }
