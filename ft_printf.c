6a92a68c61b9e0a13573e63901079e401bc6c33d
Aritz Jaber Lopes <arjaber-@c3r8s4.42urduliz.com>
2024-10-11 17:07:33 +0200

    ya se sustituye argumento por placeholder y se imprime bien %%

--- ft_printf.c
+++ ft_printf.c
@@ -6,21 +6,33 @@
 /*   By: arjaber- <arjaber-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 16:11:06 by arjaber-          #+#    #+#             */
-/*   Updated: 2024/10/11 16:44:54 by arjaber-         ###   ########.fr       */
+/*   Updated: 2024/10/11 17:07:07 by arjaber-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include <unistd.h>
 
-void	ft_print_arg(char c)
+void	ft_print_arg(char c, int *counter)
 {
-	write(1, &"k", 1);
+	write(1, &"[argumento]", 11);
+	*counter = *counter + 1;
 }
 
 int	is_arg(char c)
 {
-	
+	int	counter;
+	char	*s;
+
+	s = "cspdiuxX";
+	counter = 0;
+	while (s[counter])
+	{
+		if (s[counter] == c)
+			return (1);
+		counter++;
+	}
+	return (0);
 }
 
 int ft_printf(char const *format, ...)
@@ -32,10 +44,12 @@ int ft_printf(char const *format, ...)
 		return (1);
 	while (format[counter])
 	{
-		if (format[counter] == '%' && is_arg(format[counter + 1]))
-			ft_print_arg(format[counter++]);
-		else
+		if (format[counter] != '%')
 			write(1, &format[counter], 1);
+		else if (format[counter] == '%' &&  is_arg(format[counter + 1]))
+			ft_print_arg(format[counter + 1], &counter);
+		else if (format[counter] == '%' &&  format[counter + 1] == '%')
+			write(1, "%", 1);
 		counter++;
 	}
 	return (1);
