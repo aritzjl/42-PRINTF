8bc96c4c336786b6be9512e36456317b1f955c02
Aritz JL <aritzzjl@gmail.com>
2024-10-16 19:37:52 +0200

    fix: library path

--- aux/ft_ptr.c
+++ aux/ft_ptr.c
@@ -6,11 +6,11 @@
 /*   By: aritz <aritz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 16:46:57 by arjaber-          #+#    #+#             */
-/*   Updated: 2024/10/16 09:03:41 by aritz            ###   ########.fr       */
+/*   Updated: 2024/10/16 19:37:35 by aritz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "../ft_printf.h"
 
 int	ft_put_ptr(void *ptr)
 {
--- aux/ft_str.c
+++ aux/ft_str.c
@@ -6,11 +6,11 @@
 /*   By: aritz <aritz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 16:46:57 by arjaber-          #+#    #+#             */
-/*   Updated: 2024/10/16 09:03:42 by aritz            ###   ########.fr       */
+/*   Updated: 2024/10/16 19:37:40 by aritz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "../ft_printf.h"
 
 int	ft_putchar(char c)
 {
